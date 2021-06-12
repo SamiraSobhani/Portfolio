76b27a81d1a53f15ea8b8abfca9d56d8f5885baa
Samira Sobhani <ss121280@yahoo.com>
2021-06-11 17:12:36 -0700

    Add 3 video tp project page

--- src/components/project_main/index.js
+++ src/components/project_main/index.js
@@ -4,7 +4,13 @@ import { Col, Card, Row } from "react-bootstrap";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faLink } from "@fortawesome/free-solid-svg-icons";
 import info from "../../const/project_info.json";
-// import Lightbox from "react-image-lightbox";
+import { ReactVideo } from "reactjs-media";
+import PinPalVideo from "../../assets/img/PinPalVideo.mp4";
+import PinPalPoster from "../../assets/img/pinpalPoster.JPG";
+import CoffeeShopVideo from "../../assets/img/CoffeeShopVideo.mp4";
+import CoffeeShopPoster from "../../assets/img/coffeeShop.JPG";
+import BookStoreVideo from "../../assets/img/BookStoreVideo.mp4";
+import BookStorePoster from "../../assets/img/bookstore.JPG";
 
 export default class PM extends Component {
   constructor(props) {
@@ -18,36 +24,63 @@ export default class PM extends Component {
 
   render() {
     return (
-      <Row xl={12} className="main_row">
-        {info.data.map((item) => (
-          <Col xl={4} key={item.id} className="card_col">
-            <Card className="card_main">
-              <blockquote className="blockquote mb-0 card-body">
-                <h2>{item.name}</h2>
-                <footer className="blockquote-footer">
-                  built using <strong title="Source Title">{item.tools}</strong>
-                </footer>
-              </blockquote>
-              <Card.Body>
-                <FontAwesomeIcon
-                  onClick={() =>
-                    this.setState({ isOpen: true, image: item.image })
-                  }
-                  className="icon"
-                  size="lg"
-                  // icon={faImages}
-                />{" "}
-                &nbsp;&nbsp;
-                {item.link !== false && (
-                  <Card.Link href={item.link} target="_blank">
-                    <FontAwesomeIcon className="icon" size="lg" icon={faLink} />
-                  </Card.Link>
-                )}
-              </Card.Body>
-            </Card>
-          </Col>
-        ))}
-      </Row>
+      <div className="projects">
+        <div className="projects__video">
+          <br />
+          <ReactVideo
+            poster={PinPalPoster}
+            src={PinPalVideo}
+            primaryColor="darkred"
+          />
+          <br />
+          <ReactVideo
+            poster={BookStorePoster}
+            src={BookStoreVideo}
+            primaryColor="darkred"
+          />
+          <br />
+          <ReactVideo
+            poster={CoffeeShopPoster}
+            src={CoffeeShopVideo}
+            primaryColor="darkred"
+          />
+        </div>
+        <Row className="main_row">
+          {info.data.map((item) => (
+            <Col xl={4} key={item.id} className="card_col">
+              <Card className="card_main">
+                <blockquote className="blockquote mb-0 card-body">
+                  <h2>{item.name}</h2>
+                  <footer className="blockquote-footer">
+                    built using{" "}
+                    <strong title="Source Title">{item.tools}</strong>
+                  </footer>
+                </blockquote>
+                <Card.Body>
+                  <FontAwesomeIcon
+                    onClick={() =>
+                      this.setState({ isOpen: true, image: item.image })
+                    }
+                    className="icon"
+                    size="lg"
+                    // icon={faImages}
+                  />{" "}
+                  &nbsp;&nbsp;
+                  {item.link !== false && (
+                    <Card.Link href={item.link} target="_blank">
+                      <FontAwesomeIcon
+                        className="icon"
+                        size="lg"
+                        icon={faLink}
+                      />
+                    </Card.Link>
+                  )}
+                </Card.Body>
+              </Card>
+            </Col>
+          ))}
+        </Row>
+      </div>
     );
   }
 }
